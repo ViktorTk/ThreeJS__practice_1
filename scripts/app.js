340b8b8f16269d0f9e1312125024a56dffbbba15
Viktor <viktor.avgn@yandex.ru>
2026-03-08 23:25:30 +0700

    первая тестовая отрисовка

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,33 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
-console.log(THREE)
-console.log(OrbitControls)
+// console.log(THREE)
+
+const scene = new THREE.Scene()
+
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'red' })
+
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+
+// console.log(cubeMesh)
+
+scene.add(cubeMesh)
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  30,
+)
+
+camera.position.z = 2
+
+scene.add(camera)
+
+const canvas = document.querySelector('.threejs')
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+
+renderer.render(scene, camera)
