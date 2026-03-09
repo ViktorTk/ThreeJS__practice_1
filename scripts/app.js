64b1d1f220fb9f6e5370c306d8ea9e51a220fad5
Viktor <viktor.avgn@yandex.ru>
2026-03-09 22:55:52 +0700

    добавлены оси и сетка для корректировки позиционирования объектов

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,33 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 // console.log(THREE)
 
 const scene = new THREE.Scene()
-
 // console.log(scene)
 
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'red' })
 
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-
 // console.log(cubeMesh)
+
+// cubeMesh.position.x = 1
+// cubeMesh.position.y = 1
+// cubeMesh.position.z = -1
+
+cubeMesh.position.set(1, 0.5, 1)
+
+// цвета для обозначения позиционирования по осям:
+// x - красный
+// y - зеленый
+// z - синий
+
+const axesHelper = new THREE.AxesHelper(5)
+// console.log(axesHelper)
+scene.add(axesHelper)
+
+const gridHelper = new THREE.GridHelper(10, 10)
+// console.log(gridHelper)
+gridHelper.position.set(0.5, 0, 0.5)
+scene.add(gridHelper)
 
 scene.add(cubeMesh)
 
@@ -33,6 +51,7 @@ const canvas = document.querySelector('.threejs')
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  antialias: true,
 })
 
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -42,7 +61,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-controls.autoRotate = true
+// controls.autoRotate = true
 
 // console.log(controls);
 
